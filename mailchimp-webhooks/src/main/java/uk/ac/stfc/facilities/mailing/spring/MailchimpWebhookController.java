eb8635f80d56a11378ff5b27acb64557f9324f16
Thomas 'Panda' Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2017-09-12 18:18:21 +0100

    Add Javadoc to the webhook example

--- mailchimp-webhooks/src/main/java/uk/ac/stfc/facilities/mailing/spring/MailchimpWebhookController.java
+++ mailchimp-webhooks/src/main/java/uk/ac/stfc/facilities/mailing/spring/MailchimpWebhookController.java
@@ -5,18 +5,34 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * A Spring controller that listens to webhooks from Mailchimp, it
+ * currently handles the webhook handshake and updates to the users
+ * email. It refers to the <code>mailing.webhook.key</code> key in
+ * config to create the mapping.
+ */
 @Controller
 @RequestMapping("/${mailing.webhook.key}")
 public class MailchimpWebhookController {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    @GetMapping()
+    /**
+     * A simple method to respond with a successful status which is
+     * required for Mailchimp to accept a webhook client.
+     */
+    @GetMapping
     @ResponseBody
     public void handshake() {
     }
 
-    @PostMapping()
+    /**
+     * A webhook that listens to changes to a user&apos;s email
+     * address change.
+     * @param oldEmail the old email for the user
+     * @param newEmail the new email for the user
+     */
+    @PostMapping
     @ResponseBody
     public void hook(
             @RequestParam("data[old_email]") String oldEmail,
