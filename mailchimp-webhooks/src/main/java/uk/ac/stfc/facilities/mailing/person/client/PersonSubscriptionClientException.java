4261a6302c599ea8d1c8148c243870470bac366c
Thomas 'Panda' Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2017-11-29 15:56:20 +0000

    Only maintain used exception constructors

--- mailchimp-webhooks/src/main/java/uk/ac/stfc/facilities/mailing/person/client/PersonSubscriptionClientException.java
+++ mailchimp-webhooks/src/main/java/uk/ac/stfc/facilities/mailing/person/client/PersonSubscriptionClientException.java
@@ -7,36 +7,10 @@ package uk.ac.stfc.facilities.mailing.person.client;
 public class PersonSubscriptionClientException extends Exception {
 
     /**
-     * @see Exception#Exception()
-     */
-    public PersonSubscriptionClientException() {
-    }
-
-    /**
-     * @see Exception#Exception(String)
-     */
-    public PersonSubscriptionClientException(String message) {
-        super(message);
-    }
-
-    /**
      * @see Exception#Exception(String, Throwable)
      */
     public PersonSubscriptionClientException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    /**
-     * @see Exception#Exception(Throwable)
-     */
-    public PersonSubscriptionClientException(Throwable cause) {
-        super(cause);
-    }
-
-    /**
-     * @see Exception#Exception(String, Throwable, boolean, boolean)
-     */
-    public PersonSubscriptionClientException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
