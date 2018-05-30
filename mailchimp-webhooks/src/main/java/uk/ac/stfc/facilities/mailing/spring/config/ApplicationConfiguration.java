5f7bc7455ec56872372b09ba3f704cce203d6c77
Thomas 'Panda' Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2018-05-30 12:15:59 +0100

    Improve documentation

--- mailchimp-webhooks/src/main/java/uk/ac/stfc/facilities/mailing/spring/config/ApplicationConfiguration.java
+++ mailchimp-webhooks/src/main/java/uk/ac/stfc/facilities/mailing/spring/config/ApplicationConfiguration.java
@@ -8,6 +8,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApplicationConfiguration {
 
+    /**
+     * We only want one HTTP client for the whole application as the
+     * connections are pooled.
+     *
+     * @see <a href="https://hc.apache.org/httpcomponents-client-ga/tutorial/html/fundamentals.html">Apache HttpClient Fundamentals (section 1.2.1.)</a>
+     *
+     * @return the HttpClient for the application
+     */
     @Bean
     public CloseableHttpClient client() {
         return HttpClients.createDefault();
